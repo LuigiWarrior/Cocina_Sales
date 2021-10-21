15cf1351c020a3f20fde8d73f4653b633ff58dad
VRLabvalles <victor.rivera4283@alumnos.udg.mx>
2021-10-21 12:40:48 -0500

    sistema de respuesta
    Todo funcionando

--- Assets/ModuloPreguntas/ResponderPregunta.cs
+++ Assets/ModuloPreguntas/ResponderPregunta.cs
@@ -31,4 +31,5 @@ public class ResponderPregunta : MonoBehaviour {
 		Destroy (Pregunta);
 	}
 
+  
 }
---
+++ Assets/Proyecto_CocinaVR/Scripts/Responder_vr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Responder_vr : MonoBehaviour
+    
+{
+    
+    
+    public Text canva_puntos;
+    public int puntosPorRespuesta = 0;
+    public GameObject Pregunta;
+    public GameObject Bien;
+    public GameObject Mal;
+
+    public static int puntos;
+
+   
+    void Start()
+    {
+        //Bien.SetActive(false);
+        //Mal.SetActive(false);
+        puntos = 0;
+    }
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        puntos += puntosPorRespuesta;
+
+        //canva_puntos.text = "puntos: " + puntos;
+        if (puntos == 1)
+        {
+            Bien.SetActive(true);
+            puntos = 0;
+        }
+        else
+        {
+            Mal.SetActive(true);
+            puntos = 0;
+        }
+        //canva_puntos.text = "puntos: " + puntos;
+        Destroy(Pregunta);
+    }
+}
