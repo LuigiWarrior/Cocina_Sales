e7db77f7ff16e9b1bf4683b8d85cf3a410c9f7e1
VRLabvalles <victor.rivera4283@alumnos.udg.mx>
2021-11-16 14:58:14 -0600

    Instrucciones diagnostico bien

---
+++ Assets/Proyecto_CocinaVR/Scripts/Aparecer_instruccion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aparecer_instruccion: MonoBehaviour
+{
+
+    public GameObject Display;
+    public float tiempoMaximo = 0;
+    private int bandera = 0; 
+  
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.tag == "Untagged")
+        {
+            if (bandera==0)
+            {
+                Display.SetActive(true);
+                Invoke("activar", tiempoMaximo);
+                bandera++;
+            }
+           
+        }
+        
+
+    }
+
+    private void activar()
+    {
+        Display.SetActive(false);
+        //Display2.SetActive(true);  
+    }
+
+}
---
+++ Assets/Proyecto_CocinaVR/Scripts/Instrucciones_boton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrucciones_boton : MonoBehaviour
+{
+    public GameObject Display;
+    public GameObject Display2;
+    
+    private int bandera = 0;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Untagged")
+        {
+            if (bandera == 0)
+            {
+                Display.SetActive(true);
+                Display2.SetActive(true);
+               
+            }
+            bandera++;
+        }
+
+
+    }
+
+}
